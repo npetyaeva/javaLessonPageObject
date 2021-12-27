fa2518c157e47b90a98f135ae73c43128496ac9f
Natalia Petyaeva <npetyaeva@gmail.com>
2021-12-28 01:39:29 +0300

    Modified code

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -8,13 +8,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
-    private final SelenideElement header = $("[data-test-id='dashboard']");
+    private final SelenideElement header = $("h1"); //[data-test-id='dashboard']");
     private final ElementsCollection cards = $$("ul div[data-test-id]");
     private final ElementsCollection addButton = $$("ul button");
     private final SelenideElement balanceField = $("[data-test-id='amount'] input");
     private final SelenideElement cardFromField = $("[data-test-id='from'] input");
     private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
-    private final SelenideElement errorPopup = $("[data-test-id='error-notification']");
+    private final SelenideElement errorPopup = $("[data-test-id='error-notification'] .notification__content");
 
     private final String startBalance = "баланс:";
     private final String finishBalance = "р.";
@@ -26,14 +26,34 @@ public class DashboardPage {
     public boolean cashIn(int amount, int numberCardFrom, int numberCardTo) {
         int balanceFrom = getCardBalance(cards.get(numberCardFrom - 1));
         int balanceTo = getCardBalance(cards.get(numberCardTo - 1));
-        //System.out.println(cards);
         addButton.get(numberCardTo - 1).click();
         balanceField.setValue(String.valueOf(amount));
         cardFromField.setValue("5559 0000 0000 000" + numberCardFrom);
         transferButton.click();
-        //System.out.println(cards);
         return (balanceFrom - amount == getCardBalance(cards.get(numberCardFrom - 1))
                 && balanceTo + amount == getCardBalance(cards.get(numberCardTo - 1)));
+    }
+
+    public String fieldAmountEmpty(int numberCardFrom, int numberCardTo) {
+        addButton.get(numberCardTo - 1).click();
+        cardFromField.setValue("5559 0000 0000 000" + numberCardFrom);
+        transferButton.click();
+        return header.shouldBe(visible).text();
+    }
+
+    public String fieldCardFromEmpty(int amount, int numberCardTo) {
+        addButton.get(numberCardTo - 1).click();
+        balanceField.setValue(String.valueOf(amount));
+        transferButton.click();
+        return errorPopup.shouldBe(visible).text();
+    }
+
+    public String fieldCardInvalid(int amount, int numberCardTo) {
+        addButton.get(numberCardTo - 1).click();
+        balanceField.setValue(String.valueOf(amount));
+        cardFromField.setValue("5559 1111 1111 1111");
+        transferButton.click();
+        return errorPopup.shouldBe(visible).text();
     }
 
     private int getCardBalance(SelenideElement card) {
--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -4,33 +4,50 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MoneyTransferTest {
+    private DashboardPage dashboardPage;
+
     @BeforeEach
     void setUp() {
-        Selenide.open("http://localhost:9999/");
-    }
 
-    @Test
-    void shouldTransferMoneyFromFirstToSecondCards() {
+        Selenide.open("http://localhost:9999/");
         var clientInfo = DataHelper.getClientInfo();
         var loginPage = new LoginPage();
         var verifyPage = loginPage.validLogin(clientInfo);
         var verifyCode = DataHelper.getVerificationCode();
-        var dashboardPage = verifyPage.validVerify(verifyCode);
+        dashboardPage = verifyPage.validVerify(verifyCode);
+    }
+
+    @Test
+    void shouldTransferMoneyFromFirstToSecondCards() {
         assertTrue(dashboardPage.cashIn(1500, 1, 2));
     }
 
     @Test
     void shouldTransferMoneyFromSecondToFirstCards() {
-        var clientInfo = DataHelper.getClientInfo();
-        var loginPage = new LoginPage();
-        var verifyPage = loginPage.validLogin(clientInfo);
-        var verifyCode = DataHelper.getVerificationCode();
-        var dashboardPage = verifyPage.validVerify(verifyCode);
         assertTrue(dashboardPage.cashIn(1500, 2, 1));
+    }
+
+    @Test
+    void shouldCheckIfFieldAmountEmpty() {
+        var text = dashboardPage.fieldAmountEmpty(1, 2);
+        assertTrue("Ваши карты".contains(text));
+    }
+
+    @Test
+    void shouldCheckIfFieldCardFromEmpty() {
+        var text = dashboardPage.fieldCardFromEmpty(1500, 1);
+        assertTrue("Ошибка! Произошла ошибка".contains(text));
+    }
+
+    @Test
+    void shouldCheckIfFieldCardInvalid() {
+        var text = dashboardPage.fieldCardInvalid(1500, 1);
+        assertTrue("Ошибка! Произошла ошибка".contains(text));
     }
 }
